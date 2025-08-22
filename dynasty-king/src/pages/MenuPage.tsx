f98a93898731fc896c88105a07547d9015518ffb
lukegrady1 <lukegrady9@outlook.com>
2025-08-22 15:06:26 -0400

    update

--- dynasty-king/src/pages/MenuPage.tsx
+++ dynasty-king/src/pages/MenuPage.tsx
@@ -17,10 +17,19 @@ const DEFAULT_TABLES = [
   "soups",
   "fried_rice",
   "lo_mein",
-  // "rice",
-  // "vegetables",
-  // "desserts",
-  // "beverages",
+  "chow_fun_mei_fun",
+  "chow_mein",
+  "chop_suey",
+  "egg_foo_young",
+  "pork",
+  "poultry",
+  "beef",
+  "seafood",
+  "sweet_and_sour",
+  "vegetarian_dishes",
+  "house_combo",
+  "chefs_specialties",
+  "sides",
 ];
 
 const toTitle = (s: string) =>
